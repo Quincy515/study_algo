56f7c50053db683abc46b691b5323a314343e6d6
custertian <custertian@users.noreply.github.com>
2019-07-23 22:40:48 +0800

    3.1 图的深度优先搜索改进和基于邻接矩阵的深度优先搜索

--- graph/chapter3.graph_DFS/adjMatrixDFS.go
+++ graph/chapter3.graph_DFS/adjMatrixDFS.go
@@ -6,7 +6,7 @@ import (
 	Graph "github.com/custergo/study_algo/graph/utils"
 )
 
-func GraphDFS(G *Graph.AdjList) ([]int, []int) {
+func GraphDFS(G *Graph.AdjMatrix) ([]int, []int) {
 	var pre []int                    // 存放先序遍历结果
 	var post []int                   // 存放后序遍历结果
 	visited := make(map[int]bool, 0) // 记录每个节点是否被遍历过
@@ -18,14 +18,13 @@ func GraphDFS(G *Graph.AdjList) ([]int, []int) {
 	return pre, post
 }
 
-func dfs(v int, G *Graph.AdjList, pre *[]int, post *[]int, visited map[int]bool) {
+func dfs(v int, G *Graph.AdjMatrix, pre *[]int, post *[]int, visited map[int]bool) {
 	visited[v] = true
 	*pre = append(*pre, v) // 图的深度优先遍历的先序遍历
 
-	for w := G.Adjacency(v).Front(); w != nil; w = w.Next() {
-		ww := w.Value.(int) // 遍历顶点v的所有相邻点
-		if !visited[ww] {   // 如果没有遍历过，就递归调用
-			dfs(ww, G, pre, post, visited)
+	for _, w := range G.Adjacency(v) {
+		if !visited[w] { // 如果没有遍历过，就递归调用
+			dfs(w, G, pre, post, visited)
 		}
 	}
 
@@ -33,7 +32,7 @@ func dfs(v int, G *Graph.AdjList, pre *[]int, post *[]int, visited map[int]bool)
 }
 
 func main() {
-	if g, err := Graph.NewAdjList("g.txt"); err == nil {
+	if g, err := Graph.NewAdjMatrix("g.txt"); err == nil {
 		pre, post := GraphDFS(g)
 		fmt.Println("先序遍历结果: ", pre)  // 先序遍历结果: [0 1 3 2 6 4 5]
 		fmt.Println("后序遍历结果: ", post) // 后序遍历结果: [6 2 3 4 1 0 5]
---
+++ graph/utils/adjMatrix.go
@@ -0,0 +1,129 @@
+package Graph
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+type AdjMatrix struct {
+	V   int
+	E   int
+	adj [][]int
+}
+
+func NewAdjMatrix(name string) (*AdjMatrix, error) {
+	// 1. 读取文件
+	result := make([]string, len(name))
+	if contents, err := ioutil.ReadFile(name); err == nil {
+		result = strings.Fields(string(contents))
+	}
+
+	// 2. 根据顶点V声明二维矩阵
+	V, err := strconv.Atoi(result[0])
+	if V < 0 || err != nil {
+		return nil, errors.New("获取顶点V失败，确保V为非负整数\n")
+	}
+	adj := make([][]int, 0)
+	for i := 0; i < V; i++ {
+		tmp := make([]int, V)
+		adj = append(adj, tmp)
+	}
+
+	// 3. 根据边E来构造二维矩阵
+	E, err := strconv.Atoi(result[1])
+	if E < 0 || err != nil {
+		return nil, errors.New("获取顶点V失败，确保V为非负整数\n")
+	}
+	for i := 0; i < 2*E; i++ {
+		if i%2 == 0 {
+			a, _ := strconv.Atoi(result[2+i])
+			validateMatrixVertex(a, V) // 判断顶点是否合法
+			b, _ := strconv.Atoi(result[3+i])
+			validateMatrixVertex(b, V)
+
+			if a == b {
+				return nil, errors.New("检测到自环边!\n")
+			}
+			if adj[a][b] == 1 {
+				return nil, errors.New("检测到平行边!\n")
+			}
+
+			adj[a][b] = 1
+			adj[b][a] = 1
+		}
+	}
+
+	return &AdjMatrix{
+		V:   V,
+		E:   E,
+		adj: adj,
+	}, nil
+}
+
+// 返回图有几个顶点
+func (a *AdjMatrix) Vertex() int {
+	return a.V
+}
+
+// 返回图有几个边
+func (a *AdjMatrix) Edge() int {
+	return a.E
+}
+
+// 判断两个顶点之间是否存在一条边
+func (a *AdjMatrix) HasEdge(v, w int) bool {
+	validateMatrixVertex(v, a.V)
+	validateMatrixVertex(w, a.V)
+	return a.adj[v][w] == 1
+}
+
+// 返回v顶点的相邻的边
+func (a *AdjMatrix) Adjacency(v int) []int {
+	validateMatrixVertex(v, a.V) // 判断用户传入的顶点v是合法的
+	res := make([]int, 0)
+	for i := 0; i < a.V; i++ {
+		if a.adj[v][i] == 1 {
+			res = append(res, i)
+		}
+	}
+	return res
+}
+
+// 求一个顶点的度degree
+func (a *AdjMatrix) Degree(v int) int {
+	return len(a.Adjacency(v))
+}
+
+// 打印出邻接表表示的图
+func (a *AdjMatrix) Print() {
+	fmt.Printf("V: %d, E: %d\n", a.V, a.E)
+	for i := 0; i < a.V; i++ {
+		for j := 0; j < a.V; j++ {
+			fmt.Printf("%d ", a.adj[i][j])
+		}
+		fmt.Println()
+	}
+}
+
+func validateMatrixVertex(v, V int) {
+	if v < 0 || v >= V {
+		panic("文件内容行数列数与定义不相符\n")
+	}
+}
+
+// func main() {
+// 	adj, err := NewAdjMatrix("g.txt")
+// 	if err != nil {
+// 		fmt.Print(err)
+// 	}
+// 	adj.Print()
+// 	fmt.Println(adj.Vertex())      // 7
+// 	fmt.Println(adj.Edge())        // 9
+// 	fmt.Println(adj.HasEdge(0, 1)) // true
+// 	fmt.Println(adj.HasEdge(0, 4)) // false
+// 	fmt.Println(adj.Adjacency(1))  // [0 2 6]
+// 	fmt.Println(adj.Degree(0))     // 2
+// }
